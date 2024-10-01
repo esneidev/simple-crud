8f7b59c7e78e9d9fe7e88d547ad39c21e32723e6
Ibrah <practicante3@red5g.co>
2024-10-01 09:50:58 -0500

    actu

--- add.php
+++ add.php
@@ -12,24 +12,27 @@
 	<form action="addAction.php" method="post" name="add">
 		<table width="25%" border="0">
 			<tr> 
-				<td>Name</td>
+				<td>Name2</td>
 				<td><input type="text" name="name"></td>
 			</tr>
 			<tr> 
-				<td>Age</td>
+				<td>Age2</td>
 				<td><input type="text" name="age"></td>
 			</tr>
 			<tr> 
-				<td>Email</td>
+				<td>Email2</td>
 				<td><input type="text" name="email"></td>
 			</tr>
 			<tr>
-				<td>Gender</td>
+				<td>Gender2</td>
 				<td>
 					<select name="select-gener" id="select-gender">
 						<option value="">Select</option>
-						<option value="male">Male</option>
-						<option value="female">Female</option>
+						<option value="male">Male2</option>
+						<option value="female">Female2</option>
+						<option value="other">trans</option>
+						<option value="male">esneider gender</option>
+						<option value="female">Female2</option>
 						<option value="other">Other</option>
 					</select>
 				</td>
@@ -46,6 +49,11 @@
 				<td></td>
 				<td><input type="submit" email="submit" value="Add"></td>
 			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" gender="submit" value="Add"></td>
+			</tr>
+
 		</table>
 	</form>
 </body>
