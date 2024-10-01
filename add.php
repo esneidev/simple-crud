8de637c289350cce8befc6b054cc7c5cb05fb124
Esneider Roquemes <esneiderlastre@gmail.com>
2024-10-01 10:11:41 -0500

    Cambio de campo td nombre

--- add.php
+++ add.php
@@ -12,7 +12,7 @@
 	<form action="addAction.php" method="post" name="add">
 		<table width="25%" border="0">
 			<tr> 
-				<td>Name2</td>
+				<td>Nombre</td>
 				<td><input type="text" name="name"></td>
 			</tr>
 			<tr> 
